0b7a28c42decd7813cdad317a08cd462b3c6ab3f
Mateo-Rasso <Mateo.Rasso@endava.com>
2022-04-07 16:33:17 -0300

    Cambio la organizacion de los tests

--- fronteandYbackend/characters.js
+++ fronteandYbackend/characters.js
@@ -8,19 +8,13 @@ Los cambios efectuados por cada request se verifican a traves del frontend
 
 import RestoolApp from '../pageObjects/RestoolApp'; // Ruta desde este archivo hasta el archivo donde tengo mis clases (sin la extension .js)
 
-describe("RESTool APP Cast & Characters", function() {
+describe("Verificacion de los datos del backend con el frontend utilizando una request GET", function() {
 
-    let restoolObject = new RestoolApp(); 
+    let restoolObject = new RestoolApp();
 
-    //let uniqueId = restoolObject.createId();
+    it("Verificar que pueda obtener todos los personajes con un GET y que se encuentren en el frontend", () => {
 
-    let uniqueId = Date.now().toString();
-    
-    beforeEach(() => {
         restoolObject.visitCharacters();
-    });
-
-    it("Test GET de todos los personajes y verifico que los personajes se encuentren en el frontend", () => {
 
         cy.wait(5000)
 
@@ -52,72 +46,54 @@ describe("RESTool APP Cast & Characters", function() {
             //expect(arrayIds[index]).to.equal($el.text)  No funciona ya que "arrayIds" es un identificador de una variable y no de un array
         })
     });
+});
 
+describe('Se realizan POST y PUT de un personaje y se verifica su creacion en el frontend ', function() {
+
+    let restoolObject = new RestoolApp();
     
-    it('Test POST de un personaje y verifico que el personaje creado se muestre en el frontend', () => {
-        
+    let uniqueId = Date.now().toString();
+
+    let newName = 'Juan';
+
+    let newRealName = 'Martin';
+
+    let newLocation = 'Winterfell';
+
+    let newIsAlive = true;
+    
+    beforeEach(() => {
+
+        restoolObject.visitCharacters(); // Visitamos la pagina antes de cada test
+
         cy.request({
-                method: 'POST',
-                url: 'https://restool-sample-app.herokuapp.com/api/character',
-                form: true,
-            body:{
-                id: uniqueId,
-                thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT083A3OT8c7vXWhNFcDOM6wyHo7hXer6aIqw&usqp=CAU",
-                name: "Juan",
-                realName: "Martin",
-                location: "Winterfell",
-                isAlive: true
-                }
-            }).then(async(response) => {
-                await expect(response.status).to.eq(200)
-                expect(response.body).to.not.be.null
-                expect(response.body.id).to.be.eq(uniqueId)
-            })
+            method: 'POST',
+            url: 'https://restool-sample-app.herokuapp.com/api/character',
+            form: true,
+        body:{
+            id: uniqueId,
+            thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT083A3OT8c7vXWhNFcDOM6wyHo7hXer6aIqw&usqp=CAU",
+            name: newName,
+            realName: newRealName,
+            location: newLocation,
+            isAlive: newIsAlive
+            }
+        }).then(async(response) => {
+            await expect(response.status).to.eq(200)
+            expect(response.body).to.not.be.null
+            expect(response.body.id).to.be.eq(uniqueId)
+        })
 
-        cy.reload()
-
-        cy.wait(2000)
-
-        restoolObject.scrollAndWait(2000, 'bottom', 3);
+        restoolObject.scrollAndWait(2250, 'bottom', 3);
 
         restoolObject.verifyIdOfLastCharacter(uniqueId, 'have.text'); // Verificamos que el POST se refleje en el frontend comparando el id del 
-                                                                      //ultimo personaje en la pagina, con el id del Body del POST.
+                                                                  //ultimo personaje en la pagina, con el id del Body del POST.
     });
 
-    it('Test PUT del personaje creado y verifico que las modificaciones se muestren en el frontend', () => {       
-        
-        cy.request({
-            method: 'PUT',
-            url: 'https://restool-sample-app.herokuapp.com/api/character/' + uniqueId,
-            body:{
-                id: uniqueId,
-                thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT083A3OT8c7vXWhNFcDOM6wyHo7hXer6aIqw&usqp=CAU',
-                name: 'Juan',
-                realName: 'Martin',
-                location: 'Beyond the wall',
-                isAlive: true
-                }
-            }).then((response) => {
-                expect(response.status).to.be.eq(200)
-                expect(response.body).to.not.be.null
-                expect(response.body).to.be.eq('ok')
-                })
+    afterEach(() => {
 
-    // -------------------------- Comprobamos que el personaje haya sido editado comparando el backend con el frontend para verificar la request de tipo PUT ---------------------------------------
+// ------------------------- Enviamos una request de tipo DELETE para eliminar el personaje creado --------------------------------------------------------------
 
-        cy.reload() // Refrescamos la pagina para verificar que los cambios realizados con el PUT se reflejen en el frontend 
-        
-        cy.wait(2000)
-
-        restoolObject.scrollAndWait(2000, 'bottom', 2);
-        
-        restoolObject.verifyLocationOfLastCharacter('Beyond the wall');
-    });
-
-    // ------------------------- Enviamos una request de tipo DELETE para eliminar el personaje creado ---------------------------------------------------------------
-
-    it('Test DELETE del personaje creado', () => {  // Mismo delete que el anterior pero en otro test. 
-        
         cy.wait(2000)
 
         cy.request({
@@ -130,11 +106,43 @@ describe("RESTool APP Cast & Characters", function() {
         })
 
         cy.reload() // Refrescamos la pagina para verificar que los cambios realizados con el DELETE se reflejen en el frontend
-
-        cy.wait(1000)
         
-        restoolObject.scrollAndWait(2000, 'bottom', 2);
+        restoolObject.scrollAndWait(2500, 'bottom', 2);
 
         restoolObject.verifyIdOfLastCharacter(uniqueId, 'not.have.text') // Verificamos que el ultimo personaje en la pagina no sea el que creamos, ya que deberia haber sido eliminado con la request DELETE enviada
+    });
+
+    it('Verificacion de que el POST de un personaje se efectue y valido su creacion desde el frontend', () => {
+        // En este test se ejecutaria el POST del beforeEach. La idea es poder probar el POST por separado para ver si funciona y despues realizar otro POST para probar el PUT
+    });
+
+    it('Realizo un PUT de un personaje creado y valido la modificacion de su "location" a traves del frontend', () => {       
+        
+        newLocation = 'Beyond the wall';
+
+        cy.request({
+            method: 'PUT',
+            url: 'https://restool-sample-app.herokuapp.com/api/character/' + uniqueId,
+            body:{
+                id: uniqueId,
+                thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT083A3OT8c7vXWhNFcDOM6wyHo7hXer6aIqw&usqp=CAU',
+                name: newName,
+                realName: newRealName,
+                location: newLocation,
+                isAlive: newIsAlive
+                }
+            }).then((response) => {
+                expect(response.status).to.be.eq(200)
+                expect(response.body).to.not.be.null
+                expect(response.body).to.be.eq('ok')
+                })
+
+    // -------------------------- Comprobamos que el personaje haya sido editado comparando el backend con el frontend para verificar la request de tipo PUT ---------------------------------------
+
+        cy.reload() // Refrescamos la pagina para verificar que los cambios realizados con el PUT se reflejen en el frontend 
+
+        restoolObject.scrollAndWait(2250, 'bottom', 2);
+        
+        restoolObject.verifyLocationOfLastCharacter('Beyond the wall');
     });
 }); 
